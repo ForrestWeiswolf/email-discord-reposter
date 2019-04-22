e59264a570702dbb51517687a2ce8665d14bc8aa
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-04-22 11:40:28 -0400

    listMessageIds returns a promise

--- getMessages.js
+++ getMessages.js
@@ -3,15 +3,20 @@ const authorize = require('./authorizeGoogleAPI.js')
 
 function listMessageIds(auth) {
   const gmail = google.gmail({ version: 'v1', auth })
-  gmail.users.messages.list(
-    {
-      userId: 'me',
-    },
-    (err, res) => {
-      if (err) return console.log('The API returned an error: ' + err)
-      console.log(res.data.messages.map(message => message.id))
-    }
-  )
+
+  return new Promise((resolve, reject) => {
+    gmail.users.messages.list(
+      {
+        userId: 'me',
+      },
+      (err, res) => {
+        if (err) reject(err)
+        resolve(res.data.messages.map(message => message.id))
+      }
+    )
+  })
 }
 
-authorize().then(listMessageIds)
+authorize()
+  .then(listMessageIds)
+  .then(console.log)
