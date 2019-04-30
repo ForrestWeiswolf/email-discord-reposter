17dd92f6c13696b35e530e17813de4e462857311
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-04-30 12:12:26 -0400

    Repost emails to discord

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const fs = require('fs')
 const authorize = require('./authorizeGoogleAPI')
 const { listMessages } = require('./getMessages')
 const processMessage = require('./processMessage')
+const DiscordBot = require('./DiscordBot')
 require('./secrets.js')
 
+const bot = new DiscordBot(process.env.POST_HOOK)
+
 authorize()
-  .then(auth =>
-    listMessages(auth, process.env.GMAIL_QUERY)
-  )
+  .then(auth => listMessages(auth, process.env.GMAIL_QUERY))
   .then(messages => messages.map(processMessage))
   .then(messages => {
-    fs.writeFile('messages.json', JSON.stringify(messages), err => {
-      if (err) throw err
-      console.log('The file has been saved!')
+    messages.forEach(message => {
+      bot
+        .repostMessage(message.from, message.subject, message.payload)
+        .catch(console.log)
     })
   })
   .catch(err => console.error(err))
