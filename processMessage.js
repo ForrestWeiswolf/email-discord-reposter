17d955537870657c5aa14082f469f46eacc46690
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-04-30 10:09:16 -0400

    Copy removeQuotes and removeEnd from previous version

--- processMessage.js
+++ processMessage.js
@@ -13,11 +13,42 @@ function processMessage(message) {
     header => header.name === 'From'
   ).value
 
+  const isReply = subject.indexOf('Re:') === 0
+
+  const payload = convertPayload(message.data.payload)
+
   return {
     subject,
     from,
-    payload: convertPayload(message.data.payload),
+    payload: isReply ? removeQuotes(payload) : removeEnd(payload),
   }
+}
+
+const quoteStart = /On \w{3,}, \w{3,} \d\d?,[^\n]+ [PA]M [^\n]+ wrote:/g
+
+const quoteEnd = /--\s+You received this message because you are subscribed(.\n?)+ ?((groups.google.com\/d\/optout\.)|(\+unsubscribe@googlegroups.com\.))$/
+
+function removeQuotes(text) {
+  text = text.replace(/\s+$/, '')
+
+  while (quoteEnd.test(text) && quoteStart.test(text)) {
+    let matches = text.match(quoteStart)
+    let lastQuoteStart = text.lastIndexOf(matches[matches.length - 1])
+    text = text.slice(0, lastQuoteStart)
+  }
+
+  text = text.replace(/\s+$/, '')
+  return text
+}
+
+function removeEnd(text) {
+  text = text.replace(/\s+$/, '')
+
+  text = text.replace(quoteEnd, '')
+
+  text = text.replace(/\s+$/, '')
+
+  return text
 }
 
 module.exports = processMessage
