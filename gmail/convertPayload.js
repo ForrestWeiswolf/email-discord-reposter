c8c192d0b6b6cfc1b1bf1c6dbf9ee915456031c0
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-05-07 12:33:49 -0400

    Add comments on Turndown rules

--- gmail/convertPayload.js
+++ gmail/convertPayload.js
@@ -1,19 +1,24 @@
+// Turndown is used to convert HTML to markdown.
 const Turndown = require('turndown')
 const turndownService = new Turndown()
   .addRule('link', {
+    // Discord only supports a limited subset of markdown, which doesn't include inline links.
+    // So when there's an <a></a> tag we'll put the href URL in parentheses after the content.
     filter: 'a',
     replacement: function(content, node) {
       return `${content} (${node.getAttribute('href')})`
     },
   })
   .addRule('linebreak', {
+    // Turndown ought to convert <br /> to a newline by default, in my opinion
+    // but it doesn't, so we have this rule to handle that.
     filter: 'br',
     replacement: function(content, node) {
       return '\n'
     },
   })
 
-/**
+  /**
  * Converts the payload of a message resource
  * (https://developers.google.com/gmail/api/v1/reference/users/messages#resource)
  * returned from the GMail API from an object containing base64 encoded HTML to
@@ -30,6 +35,9 @@ function convertPayload(payload) {
     // Decode from base64:
     const utf8String = Buffer.from(payload.body.data, 'base64')
       .toString('utf-8')
+      // Yes, we're converting newlines to <br> and then will have Turndown convert them back
+      // There was an issue with it stripping out all newlines in some messages,
+      // and this is a workaround.
       .replace(/[\n\r]/g, '<br>')
 
     // convert HTML to markdown:
