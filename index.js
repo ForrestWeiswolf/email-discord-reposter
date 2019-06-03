32b8447928df7188681113222ba9467eb84c6d31
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-06-03 19:46:01 -0400

    Try to not get rate limited

--- index.js
+++ index.js
@@ -6,18 +6,32 @@ const DiscordBot = require('./discord/DiscordBot')
 if (process.env.NODE_ENV !== 'production') require('./secrets.js')
 const bot = new DiscordBot(process.env.POST_HOOK, process.env.NEW_THREAD_ROLE)
 
+function forEachWait(arr, func, wait) {
+  let idx = 0
+  let interval = setInterval(() => {
+    func(arr[idx])
+    idx++
+    if (idx === arr.length) {
+      clearInterval(interval)
+    }
+  }, wait)
+}
+
 authorize()
   .then(auth => listMessages(auth, process.env.GMAIL_QUERY))
   // sort them into chronological order
   .then(messages => messages.sort((a, b) => b.internalDate - a.internalDate))
   .then(messages => messages.map(processMessage))
   .then(messages => {
-    // The messages are in most-to-least recent order, I believe,
-    // So we'll reverse to put them in chronological order
-    messages.reverse().forEach(message => {
-      bot
-        .repostMessage(message.from, message.subject, message.payload)
-        .catch(console.log)
-    })
+    // Wait a second between messages so as not to get rate limited by Discord
+    forEachWait(
+      messages,
+      message => {
+        bot
+          .repostMessage(message.from, message.subject, message.payload)
+          .catch(console.log)
+      },
+      1000
+    )
   })
   .catch(err => console.error(err))
